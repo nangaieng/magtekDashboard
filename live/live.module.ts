00ca0cce7da395012a09bb000fd82bd1103d3534
Daniel Magesa <mage8942@gmail.com>
2018-03-15 01:54:19 +0300

    Add files via upload

--- live/live.module.ts
+++ live/live.module.ts
@@ -1,15 +1,16 @@
-SecurityCamerasComponentimport { NgModule } from '@angular/core';
 import { NgModule } from '@angular/core';
 import { ThemeModule } from '../../@theme/theme.module';
 import { HttpClientModule } from '@angular/common/http';
-import { SecurityCamerasComponent } from './security-cameras/security-cameras.component';
+
 
 @NgModule({
   imports: [
 	ThemeModule,
 	HttpClientModule,
   ],
-  declarations: [SecurityCamerasComponent]
+  exports:[],
+  declarations: [
+  ]
 })
 export class LiveModule { }
 
